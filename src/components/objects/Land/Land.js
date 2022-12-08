6f5bb0b2cc3134546a3fdaeaaa96cee7d8ea04f7
Henry Knoll <hknoll@princeton.edu>
2022-12-08 17:32:34 -0500

    Better ground

--- src/components/objects/Land/Land.js
+++ src/components/objects/Land/Land.js
@@ -1,4 +1,4 @@
-import { Group } from 'three';
+import { Group, PlaneGeometry, DoubleSide, MeshBasicMaterial, Mesh } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import MODEL from './land.gltf';
 
@@ -11,9 +11,17 @@ class Land extends Group {
 
         this.name = 'land';
 
+        /*
         loader.load(MODEL, (gltf) => {
             this.add(gltf.scene);
         });
+        */
+
+        const geometry = new PlaneGeometry(10, 10);
+        const material = new MeshBasicMaterial({color: 0x4B8E46, side: DoubleSide});
+        const plane = new Mesh(geometry, material);
+        plane.rotation.set(Math.PI / 2, 0, 0);
+        this.add(plane);
     }
 }
 
