f5a312b949813feafaa1d4ad5a88ae42e9ba0c9d
zeligsongit <zeligson@princeton.edu>
2022-12-11 16:06:02 -0500

    added game start gunction

--- src/app.js
+++ src/app.js
@@ -34,53 +34,59 @@ div.style.display= "none";  // comment out this line to show the homescreen
 div.style.position= 'fixed';
 div.style.textAlign= 'center';
 div.style.zIndex = '100';
-div.style.left= '25%';
+div.style.left= '20%';
 div.style.color= 'white';
-div.style.top= '15%';
+div.style.top= '10vh';
 div.style.fontSize= '20px';
 div.style.fontFamily= 'Arial';
 div.style.width= "50%";
-div.style.height= "70%";
+div.style.height= "fit-content";
 div.style.backgroundColor= "#404040";
-div.style.padding= "20px";
 div.style.paddingLeft= "40px";
 div.style.paddingRight= "40px";
+div.style.boxShadow= "0px 0px 40px 8px black";
 div.innerHTML=`
 <p style='color: orange; font-weight: bolder; font-size: 40px'>
     Welcome to Lawnmower Lunacy
 </p>
 <p style='text-align: left'>Gameplay Directions:
     <ul style='text-align: left'>
-        <li>Direction 1</li>
-        <li>Direction 2</li>
-        <li>Direction 3</li>
-        <li>Direction 4</li>
+        <li>Use the &#8593; and &#8595; keyboard keys to speed up and slown down.</li>
+        <li>Use the &#8592; and &#8594; keyboard keys to change direction.</li>
+        <li>Cut as much grass as you can before time runs out!!</li>
     </ul>
 </p>
-<button id="startGame" style="background-color: red; color: white; border: none; width: 50%; border-radius: 8px; height: 50px; font-size: 25px;">
+<br>
+<button id="countdownSequence" style="cursor: pointer; background-color: red; color: white; border: none; width: fit-content; height: 50px; font-size: 25px;">
     Mow That Lawn!!
 </button>
+<br>
+<br>
 `
 const countdown = document.createElement('div');
 countdown.innerHTML=`
-<span id="three" style="font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 50%; top: 25%;">3</span>
-<span id="two" style="font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 50%; top: 25%;">2</span>
-<span id="one" style="font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 50%; top: 25%;">1</span>
-<span id="go" style="font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 32%; top: 25%;">Go!!</span>
+<span id="three" style="width: 100vw; text-align: center; font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 0; top: 25%;">3</span>
+<span id="two" style="width: 100vw; text-align: center; font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 0; top: 25%;">2</span>
+<span id="one" style="width: 100vw; text-align: center; font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 0; top: 25%;">1</span>
+<span id="go" style="width: 100vw; text-align: center; font-family: arial; visibility: hidden; color: red; font-size: 300px; line-height: 300px; z-index: 100; position: fixed; left: 0; top: 25%;">Go!!</span>
 
 `
 document.body.append(div);
 document.body.append(countdown);
-document.getElementById("startGame").addEventListener("click", startGame);
+document.getElementById("countdownSequence").addEventListener("click", countdownSequence);
 console.log(document)
 
-function startGame(){
-    console.log("Game Started!!");
+function countdownSequence(){
     document.getElementById("startScreen").style.display="none";
     document.getElementById("three").style.visibility="visible";
     setTimeout((() => {document.getElementById("three").style.visibility="hidden"; document.getElementById("two").style.visibility="visible"}), 1500);
     setTimeout((() => {document.getElementById("two").style.visibility="hidden"; document.getElementById("one").style.visibility="visible"}), 3000);
     setTimeout((() => {document.getElementById("one").style.visibility="hidden"; document.getElementById("go").style.visibility="visible"}), 4500);
+    setTimeout((() => {document.getElementById("go").style.visibility="hidden"; startGame()}), 6000);
+}
+
+function startGame(){
+    console.log("Game Started!!");
 }
 
 // Set up controls
