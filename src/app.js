49a0d2a9ac094feaf620f10a4f780830c14cb7e4
Brian Tieu <btieu@princeton.edu>
2022-12-16 16:16:22 -0500

    move audio back

--- src/app.js
+++ src/app.js
@@ -119,19 +119,19 @@ countdown.innerHTML = `
 
 document.body.append(div);
 document.body.append(countdown);
-const audioPlay = document.createElement('script');
-audioPlay.innerHTML =
-`
-var audio = new Audio('src/audio/background.mp3');
-    audio.volume = 0.5;
-    audio.addEventListener('ended', function() {
-        console.log("Audio ended");
-        this.currentTime = 0;
-        this.play();
-    }, false);
-    audio.play();
-`
-document.body.append(audioPlay)
+// const audioPlay = document.createElement('script');
+// audioPlay.innerHTML =
+// `
+// var audio = new Audio('src/audio/background.mp3');
+//     audio.volume = 0.5;
+//     audio.addEventListener('ended', function() {
+//         console.log("Audio ended");
+//         this.currentTime = 0;
+//         this.play();
+//     }, false);
+//     audio.play();
+// `
+// document.body.append(audioPlay)
 
 document
     .getElementById('countdownSequence')
@@ -219,14 +219,14 @@ function startGame() {
 
     // Set up lawn mower movement
     window.addEventListener('keydown', handleKeyDown, false);
-    // var audio = new Audio('src/audio/background.mp3');
-    // audio.volume = 0.5;
-    // audio.addEventListener('ended', function() {
-    //     console.log("Audio ended");
-    //     this.currentTime = 0;
-    //     this.play();
-    // }, false);
-    // audio.play();
+    var audio = new Audio('src/audio/background.mp3');
+    audio.volume = 0.5;
+    audio.addEventListener('ended', function() {
+        console.log("Audio ended");
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    audio.play();
     setTimeout(() => {
         startTimer();
     }, 3000);
